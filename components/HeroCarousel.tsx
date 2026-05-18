1ea3ab6705c21ab5266fc17029f4dbef0d9399b0
Aidan <eden8842@gmail.com>
2026-05-19 06:19:44 +0800

    fix: hero uses 100svh on iOS Safari so chevron clears address bar

--- components/HeroCarousel.tsx
+++ components/HeroCarousel.tsx
@@ -19,7 +19,7 @@ export default function HeroCarousel({
   const fade = 100 / duration;
 
   return (
-    <section className="relative h-screen w-full overflow-hidden">
+    <section className="relative h-screen w-full overflow-hidden" style={{ height: '100svh' }}>
       {slides.map((slide, index) => (
         <div
           key={slide.src}
